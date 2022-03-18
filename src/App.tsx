66a83499ad7b8ea38311cf4f055c854c7cf1097c
yaogengzhu <455947455@qq.com>
2022-03-18 22:37:36 +0800

    feat: page css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
+import React from 'react'
 import { data } from './mock'
 import './index.less'
+
+
+type IItem = {
+	title: string
+	form: string
+	time: string
+}
+const Item: React.FC<IItem> = (props) => {
+	const { title,from,time,image } = props
+	return (
+		<div className="item">
+			<div className="textContet">
+				<div>{ title }</div>
+				<div>
+					<span className="tip">{ from }</span>
+					<span className="tip one">{ time }</span>
+				</div>
+			</div>
+			<div className="img">
+				<img src={ image } alt="" />
+			</div>
+		</div>
+	)
+}
 const App = () => {
 	const { list } = data
+	console.log(list)
 	return (
-		<ul>
-			{ list.map((item) => (
-				<li key={ item.id }>{ item.title }</li>
-			)) }
-		</ul>
+		<div>
+			{
+				list.map(item => (
+					<Item key={ item.id } { ...item } />
+				))
+			}
+		</div>
+		// <ul>
+		// 	{ list.map((item) => (
+		// 		<li key={ item.id }>{ item.title }</li>
+		// 	)) }
+		// </ul>
 	)
 }
 
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,13 +1,13 @@
-import Mock from 'mockjs'
+import Mock,{ Random } from 'mockjs'
 export const data = Mock.mock({
 	// 属性 list 的值是一个数组，其中含有 1 到 10 个元素
-	'list|10000': [ {
+	'list|100': [ {
 		// 属性 id 是一个自增数，起始值为 1，每次增 1
 		'id|+1': 1,
-		title: "@title(3,10)",
-		image: "@natural(0,15)",
+		title: "@ctitle(15,25)",
+		image: Random.dataImage('130x80'),
 		reads: "@natural(3,99999)",
 		from: "@ctitle(3, 10)",
-		data: "@date('yyyy-mm-dd')"
+		time: "@date('yyyy-mm-dd')"
 	} ]
 })
