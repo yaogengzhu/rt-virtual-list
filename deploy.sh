9900430f8b3e3fa91cd18487055b896b8fd6f837
yaogengzhu <455947455@qq.com>
2022-03-19 22:59:54 +0800

    change vesion

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,23 @@
-#!/usr/bin/env sh
-
-# 发生错误时终止
+# 发生任何错误时终止
 set -e
-
-# git 
-git init
-git add -A
-git commit -m 'deploy'
 
 # 构建
 npm run build
 
-# 进入构建文件夹
+# 进入输出产物文件夹
 cd dist
 
 # 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你要部署在 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yaogengzhu/rt-virtual-list.git main:gh-pages
-# https://github.com/yaogengzhu/rt-virtual-list
+git push -f git@github.com:yaogengzhu/rt-virtual-list.git master:gh-pages
 
 cd -
